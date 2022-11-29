715abaa00521dbf92d955cb97d83633c152e5db5
Bianca Fu <0509biancafu@gmail.com>
2022-11-29 01:08:11 +0000

    used route parameter

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id : req.params.id, longURL : urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
