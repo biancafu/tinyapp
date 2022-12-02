748099658bbd4bd6157b2284f88b24c2850f9241
Bianca Fu <0509biancafu@gmail.com>
2022-12-02 04:35:57 +0000

    added permission where only logged in users get to edit/delete or see the urls that are created by them. if not, sends responding error messages

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
+const { generateRandomString, getUserByEmail, urlsForUser, urlAccessDenied } = require("./helpers");
 
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+// structure of users -> id : {id, email, password : hashedPassword}
+const users = {}; 
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -18,38 +21,29 @@ const urlDatabase = {
   },
 };
 
-const users = {};
-
-
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-//can put a callback function and it would run each time a route gets called
+app.use(cookieSession({
+  name: 'session',
+  keys: ["someSecretKey", "AnotherSecretKey"]}));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-//homepage, displaying urls
+//urls homepage, displaying urls only if user is logged in
 app.get("/urls", (req, res) => {
-  const id = req.cookies["user_id"];
+  const id = req.session.user_id;
   if (id === undefined) {
-    res.send("Error: user not logged in");
+    res.redirect("/login");
     return;
   }
-  const templateVars = { urls : urlsForUser(id), user : users[id]};
-  console.log(urlsForUser(id));
+  const templateVars = { urls : urlsForUser(id, urlDatabase), user : users[id]};
   res.render("urls_index", templateVars);
 });
 
 //adding new url
 app.post("/urls", (req, res) => {
-  const id = req.cookies["user_id"];
+  const id = req.session.user_id;
   if (id === undefined) {
-    console.log("please sign in first");
     res.send("Error: please sign in first");
     return;
   }
-  console.log(req.body); // Log the POST request body to the console
   const longURL = req.body.longURL;
   urlDatabase[generateRandomString()] = { longURL : longURL, userID : id};
   res.redirect(longURL); 
@@ -57,31 +51,29 @@ app.post("/urls", (req, res) => {
 
 //create new url
 app.get("/urls/new", (req, res) => {
-  const id = req.cookies["user_id"];
-  const templateVars = {
-    user : users[id]
-  };
+  const id = req.session.user_id;
   if (id === undefined) {
-    console.log("please login first (get)");
     res.redirect("/login");
     return;
   }
+  const templateVars = {
+    user : users[id]
+  };
+
   res.render("urls_new", templateVars);
 });
 
 
 //register page
 app.get("/register", (req, res) => {
-  const id = req.cookies["user_id"];
-  const templateVars = {
-    user : users[id]
-  };
+  const id = req.session.user_id;
   if (id !== undefined) {
-    console.log("already registered");
     res.redirect("/urls");
     return;
   }
-  console.log("going to register page");
+  const templateVars = {
+    user : users[id]
+  };
   res.render("urls_register", templateVars);
 });
 
@@ -94,30 +86,28 @@ app.post("/register", (req, res) => {
   if (email === "" || password === "") {
     res.status(400).send("empty email/password!");
     return;
-  } else if (getUserByEmail(email) !== null) {
+  } else if (getUserByEmail(email, users) !== null) {
     res.status(400);
     res.send("This email has been registered!");
     return;
   }
   const id = generateRandomString();
   users[id] = { id, email, password : hashedPassword };
-  console.log("new user: ", users[id]);
-  res.cookie("user_id", id);
+  req.session.user_id = id;
   res.redirect("/urls");
   
 });
 
 //login page
 app.get("/login", (req, res) => {
-  const id = req.cookies["user_id"];
-  const templateVars = {
-    user : users[id]
-  };
+  const id = req.session.user_id;
   if (id !== undefined) {
-    console.log("already signed in");
     res.redirect("/urls");
     return;
   }
+  const templateVars = {
+    user : users[id]
+  };
   res.render("urls_login", templateVars);
 });
 
@@ -125,130 +115,95 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const inputPassword = req.body.password;
-  const user = getUserByEmail(email);
-  const hashedPassword = user.password;
-
+  const user = getUserByEmail(email, users);
   if (user === null) {
     res.status(403).send("User cannot be found");
     return;
-  } else if (!bcrypt.compareSync(inputPassword, hashedPassword)) {
+  }
+  const hashedPassword = user.password;
+  if (!bcrypt.compareSync(inputPassword, hashedPassword)) {
     res.status(403).send("Invalid password");
     return;
   }
-  console.log("login as", email, "password", inputPassword, "hash", hashedPassword);
-  res.cookie("user_id", user.id);
+  req.session.user_id = user.id;
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  const id = req.cookies["user_id"];
-  const user = users[id];
-  console.log("logout as", user);
-  res.clearCookie("user_id");
+  req.session = null;
   res.redirect("/login");
 });
 
-
+//delete url
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id;
-  const user_id = req.cookies["user_id"];
+  const url_id = req.params.id;
+  const user_id = req.session.user_id;
   if (user_id === undefined) {
     //user not longged int
     res.send("Error: please login first!");
     return;
-  } else if (urlDatabase[id] === undefined) {
+  } else if (urlDatabase[url_id] === undefined) {
     //id doesn't exist
-    res.send("Error: id does not exist");
+    res.send("Error: url id does not exist");
     return;
-  } else if (urlAccessDenied(id, user_id)) {
+  } else if (urlAccessDenied(url_id, user_id, urlDatabase)) {
     //user doens't own url
     res.send("Error: You don't have permission to delete this url");
     return;
   }
-  console.log("deleting", id, urlDatabase[id]);
-  delete urlDatabase[id];
+  delete urlDatabase[url_id];
   res.redirect("/urls");
+});
+
+//url detail page
+app.get("/urls/:id", (req, res) => {
+  const url_id = req.params.id;
+  const user_id = req.session.user_id;
+  if (user_id === undefined) {
+    res.send("Error message: user not logged in");
+    return;
+  } else if (urlAccessDenied(url_id, user_id, urlDatabase)) {
+    //url saved by other user
+    res.send("Error: you don't have permission to view this url");
+    return;
+  } else if (urlDatabase[url_id] === undefined) {
+    //invalid url
+    res.send("Error: url id does not exist");
+    return;
+  }
+  const templateVars = {id : url_id, longURL : urlDatabase[url_id]["longURL"], user : users[user_id] };
+  res.render("urls_show", templateVars);
 });
 
 //edit
 app.post("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("new URL:", req.body);
+  const url_id = req.params.id;
+  const user_id = req.session.user_id;
+
+  if (user_id === undefined) {
+    //user not longged int
+    res.send("Error: please login first!");
+    return;
+  } else if (urlDatabase[url_id] === undefined) {
+    //id doesn't exist
+    res.send("Error: url id does not exist");
+    return;
+  } else if (urlAccessDenied(url_id, user_id, urlDatabase)) {
+    //user doens't own url
+    res.send("Error: You don't have permission to edit this url");
+    return;
+  }
+
   const longURL = req.body.longURL;
-  urlDatabase[id] = {longURL, userID : req.cookies["user_id"]};
+  urlDatabase[url_id] = {longURL, userID : user_id};
   res.redirect(`/urls`);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const user_id = req.cookies["user_id"];
-  const urls = urlsForUser(user_id);
-  if (user_id === undefined) {
-    res.send("Error message: user not logged in");
-    return;
-  } else if (urlAccessDenied(id, user_id)) {
-    //url saved by other user
-    res.send("You don't have permission to view this url");
-    return;
-  } else if (urlDatabase[id] === undefined) {
-    //invalid url
-    res.send("Error: id does not exist");
-    return;
-  }
-  const templateVars = {id : id, longURL : urlDatabase[id]["longURL"], user : users[user_id] };
-  res.render("urls_show", templateVars);
-})
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!", users };
-  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-const generateRandomString = () => {
-    const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let result = '';
-
-    for (let i = 0; i < 6; i++) {
-      result += chars[Math.floor(Math.random() * chars.length)];
-    } 
-    return result;
-};
-
-const getUserByEmail = (newEmail) => {
-  for (const id in users) {
-    if (users[id].email === newEmail) {
-      return users[id];
-    }
-  }
-  return null;
-};
-
-const urlsForUser = (id) => {
-  let urls = {};
-  for (const key in urlDatabase) {
-    const userID = urlDatabase[key].userID;
-    if (userID === id) {
-      urls[key] = urlDatabase[key].longURL;
-    }
-  }
-  return urls;
-};
-
-const urlAccessDenied = (id, user_id) => {
-  //return true if u don't have permission to this url
-  const urls = urlsForUser(user_id);
-  //user doesn't have url saved in this id
-  const condition1 = !Object.keys(urls).includes(id);
-  //this id is in urlDatabase under other user_id
-  const condition2 = Object.keys(urlDatabase).includes(id);
-  return condition1 && condition2;
-}
